5713755ab5c6d7cafe848f0fb5f9c6cf609e854a
Thomas Riquier <thomasriq@MacBook-Pro-de-Thomas.local>
2025-02-06 09:12:32 +0100

    Langue / Affichage illu / Responsive

---
+++ src/js/lang.js
@@ -0,0 +1,10 @@
+const translations = {
+    fr: {
+        subtitlehome: "designeuse en devenir",
+        about: "À propos de moi"
+    },
+    en: {
+        subtitlehome: "zqdefsghezrgfh",
+        about: "About me"
+    }
+};
